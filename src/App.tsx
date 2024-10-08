13b171ee59e2be8aa34ab9d36e0568ecdbf274bd
MYWIND\117072 <dun.caiyun@relectric.cn>
2024-10-08 10:35:37 +0800

    useRef 与 ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 // useRef + ts
 
-type Props = {
-  onGetMsg?: (msg:string)=>void
-}
+import { useEffect, useRef } from "react"
 
-function Son(props:Props){
-  const { onGetMsg } = props
-  const clickHandler = () =>{
-    onGetMsg?.('this is msg')
-  }
-  return <button onClick={clickHandler}>sendMsg</button>
-}
 
+// 1. 获取dom
+// 2.稳定引用的存储器 （定时器管理）
 function App() {
-  const getMsgHandler = (msg:string) => {
-    console.log(msg);
-  }
+  const domRef = useRef<HTMLInputElement>(null)
+  const timerId = useRef<number | undefined>(undefined)
+  useEffect(()=>{
+    // 可选链 前面不为空值（null / undefined）执行点运算
+    // 类型守卫 防止出现空值点运算错误
+    domRef.current?.focus()
+
+    timerId.current = setInterval(()=>{
+      console.log('123');
+      
+    },1000)
+    return ()=>clearInterval(timerId.current)
+  },[])
+
   return (
     <>
-      this is app
-      <Son onGetMsg={(msg)=>console.log(msg)}/>
-      <Son onGetMsg={getMsgHandler}/>
+     <input ref={domRef}/>
     </>
   )
 }
